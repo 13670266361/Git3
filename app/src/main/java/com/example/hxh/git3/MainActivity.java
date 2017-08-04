5237576f61fb148e6388721aaf5478dd88f5ac3e
黄旭晖 <huangxuh@bsoft.com.cn>
2017-08-04 17:30:14 +0800

    2

--- app/src/main/java/com/example/hxh/git3/MainActivity.java
+++ app/src/main/java/com/example/hxh/git3/MainActivity.java
@@ -10,5 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    ////dada
+    ////dada22222222222222222222222222sda
 }
